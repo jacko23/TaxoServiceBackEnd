b785802e269bcbd473027cc0e5f373536a5f2992
Jackson <jackson.freire@piksel.com>
2018-11-19 18:11:42 +0100

    insert post document

--- src/main/java/taxo/service/backend/controller/ContactController.java
+++ src/main/java/taxo/service/backend/controller/ContactController.java
@@ -2,15 +2,21 @@ package taxo.service.backend.controller;
 
 
 
+import javax.validation.Valid;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import taxo.service.backend.model.Contact;
 import taxo.service.backend.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 
 
@@ -50,5 +56,10 @@ public class ContactController {
 		LOG.debug("I dati sono stati registrati");
 		
 		return contactRepository.findAll();
+	}
+	
+	@PostMapping("/contact")
+	public Mono<Contact> createContact(@Valid @RequestBody Contact contact){
+		return contactRepository.save(contact);
 	}
 }
