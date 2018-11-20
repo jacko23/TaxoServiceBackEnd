4e3867070450752a7a5595971153814015e90a5d
Jackson <jackson.freire@piksel.com>
2018-11-20 12:37:00 +0100

    Aggiunto metodo - ricerca x id e cancella usr

--- src/main/java/taxo/service/backend/controller/ContactController.java
+++ src/main/java/taxo/service/backend/controller/ContactController.java
@@ -6,12 +6,16 @@ import javax.validation.Valid;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.coyote.http11.Http11AprProtocol;
 
 import taxo.service.backend.model.Contact;
 import taxo.service.backend.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -61,5 +65,25 @@ public class ContactController {
 	@PostMapping("/contact")
 	public Mono<Contact> createContact(@Valid @RequestBody Contact contact){
 		return contactRepository.save(contact);
+	}
+	
+	@GetMapping("/contact/{id}")
+	public Mono<ResponseEntity<Contact>> getContactById(@PathVariable(value ="id") String id){
+		
+		return contactRepository.findById(id)
+				.map(savedContact -> ResponseEntity.ok(savedContact))
+				.defaultIfEmpty(ResponseEntity.notFound().build());
+		
+	}
+	
+	@DeleteMapping("/contactdel/{id}")
+	public Mono<ResponseEntity<Void>> deleteContact(@PathVariable(value = "id") String id){
+		
+		return contactRepository.findById(id)
+				.flatMap(existinContact -> 
+				contactRepository.delete(existinContact)
+					.then(Mono.just(new ResponseEntity<Void>(HttpStatus.OK)))
+					)
+					.defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
 	}
 }
