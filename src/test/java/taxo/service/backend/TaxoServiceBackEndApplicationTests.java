dc5a5f1cb378e3aa9ca6d486a1bb4b4d596b3a2e
Jackson <jackson.freire@piksel.com>
2018-11-20 18:20:40 +0100

    reactive coding

--- src/test/java/taxo/service/backend/TaxoServiceBackEndApplicationTests.java
+++ src/test/java/taxo/service/backend/TaxoServiceBackEndApplicationTests.java
@@ -2,15 +2,27 @@ package taxo.service.backend;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import taxo.service.backend.repository.ContactRepository;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TaxoServiceBackEndApplicationTests {
 
+	String uri = "http://localhost:8090";
+	@Autowired
+	ContactRepository contactRepository;
+	
 	@Test
-	public void contextLoads() {
+	public void getShowContacts() throws Exception{
+		RestTemplate restTemplate = new RestTemplate();
+		
+		String resultMDB = restTemplate.getForObject(uri+"/showcontacts", String.class);
+		System.out.println("Lista utenti : "+ resultMDB);
 	}
 
 }
